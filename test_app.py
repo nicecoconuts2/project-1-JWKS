b6a1280c8a780239a5cc7803293322c4dc59bf6f
nicecoconuts2 <105409925+nicecoconuts2@users.noreply.github.com>
2024-09-22 20:18:20 -0500

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,17 +1,16 @@
-#import pytest
-#from app import app  # Adjust this import if your main file is named differently
+#import pytest#from app import app  # Adjust this import if your main file is named differently
 
-#@pytest.fixture
-##def client():
+@pytest.fixture
+def client():
     #with app.test_client() as client:
     #    yield client
 
-#def test_jwks(client):
-   ## response = client.get('/jwks')
-    #assert response.status_code == 200
-   # assert 'keys' in response.get_json()
+def test_jwks(client):
+    response = client.get('/jwks')
+    assert response.status_code == 200
+    assert 'keys' in response.get_json()
 
-#def test_auth(client):
-   ## response = client.post('/auth')
-   # assert response.status_code == 200
-#   assert 'token' in response.get_json()
+def test_auth(client):
+    response = client.post('/auth')
+ assert response.status_code == 200
+  assert 'token' in response.get_json()
